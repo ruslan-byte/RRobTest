bc712d85f79e2978166d1ea093e2da684e5f350a
ruslan-byte <rbaroev0@gmail.com>
2023-07-31 09:51:35 +0300

    review scss

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
-F<template>
-  <div class="app container">
-    <Header />
-  </div>
+<template>
+	<div class="app container">
+		<Header />
+	</div>
 </template>
 <script>
-  import Header from '@/components/Header.vue';
-  export default {
-   components:['Header']
-  }
+	import Header from '@/components/Header.vue';
+	export default {
+	 components: ['Header']
+	}
 </script>
 
 <style lang="scss">
-  .app.container {padding-top: 60px; }
-  @media (min-width: $tablet)
-  {
-    .app.container {padding-top: 80px; }
-  }
-  @media (min-width: $desktop)
-  {
-    .app.container {padding-top: 100px; }
-  }
+	.app {padding-top: 60px; }
+	@media (min-width: $tablet)
+	{
+		.app {padding-top: 80px; }
+	}
+	@media (min-width: $desktop)
+	{
+		.app {padding-top: 100px; }
+	}
 </style>
